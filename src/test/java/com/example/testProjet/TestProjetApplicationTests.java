4ae1c18228a83954b9350abe077879dd7a806ffa
GotaKev <kflesch.pro@gmail.com>
2022-08-22 21:22:04 +0200

    ajout3

---
+++ src/test/java/com/example/testProjet/TestProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
